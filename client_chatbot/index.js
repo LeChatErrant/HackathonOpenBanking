bb1041afbe09d0a64207b7bb7c0f23d9b65f46fa
LeChatErrant <guillaume.hector@epitech.eu>
2018-11-08 10:00:48 +0000

    merge

--- client_chatbot/index.js
+++ client_chatbot/index.js
@@ -15,7 +15,7 @@ jaw.stdout.on('data', data => console.log(`JAW STDOUT :\n${data}\n`));
 jaw.stderr.on('data', data => console.log(`JAW STDERR :\n${data}\n`));
 setInterval(() => {
 	jaw.stdin.write("DAB\n");
-}, 100);
+}, 200);
 
 //Socket.IO client
 const io = require('socket.io-client');
