376fdfbf28159c48ddd8561e2f492389d15a81ab
Adam Cohen <acohen@localhost.localdomain>
2018-11-09 10:30:47 +0100

    jaw script : new flag for unplugged inmoov

--- inMoov/jaw/jaw.py
+++ inMoov/jaw/jaw.py
@@ -6,26 +6,33 @@ import tty
 import termios
 import serial
 
-jaw = serial.Serial("/dev/ttyUSB0", 9600)
+unplug = False
+
+if (len(sys.argv) > 1 and sys.argv[1] == "--unplugged") :
+    unplug = True
+
+if not unplug :
+    jaw = serial.Serial("/dev/ttyUSB0", 9600)
 jaw_pos = 0
 content = ""
 speak_mode = False
 
 def move_jaw(speak_mode) :
     global jaw_pos
+    global unplug
 
     if speak_mode == False and jaw_pos == 0 :
         return
     elif speak_mode == False :
-        jaw.write("j-30,\n".encode())
-        #print("close")
+        if not unplug :
+            jaw.write("j-30,\n".encode())
     else :
         if jaw_pos == 0 :
-            jaw.write("j30,\n".encode())
-            #print("open")
+            if not unplug :
+                jaw.write("j30,\n".encode())
         else :
-            jaw.write("j-30,\n".encode())
-            #print("close")
+            if not unplug :
+                jaw.write("j-30,\n".encode())
         jaw_pos = 0 if jaw_pos == 1 else 1
 
 while True:
