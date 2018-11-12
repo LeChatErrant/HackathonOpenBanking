c922e712e754695be3b85d1104a40adc0d805340
HECTOR Guillaume <LeChatErrant@localhost.localdomain>
2018-11-12 14:23:56 +0100

    rendez-vous-followup context reset

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -33,7 +33,7 @@ const rendezvous = (body, parameter, response) => {
 				response.fulfillmentText = body.queryResult.fulfillmentMessages[1].text.text[0];
 			}
 			response.outputContexts = body.queryResult.outputContexts;
-			response.outputContexts[1].lifespanCount = 0;
+			response.outputContexts[0].lifespanCount = 0;
 		}
 		resolve();
 	});
