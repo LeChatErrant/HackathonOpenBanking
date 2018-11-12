e5bfe119758d1ca883a168df34e91a4bc6bde77c
Adam Cohen <acohen@localhost.localdomain>
2018-11-12 14:45:18 +0100

    tracking update

--- inMoov/inMoov.py
+++ inMoov/inMoov.py
@@ -8,13 +8,7 @@ class InMoov :
     def __init__(self, neck_coef, eyes_sens) :
         self.head = serial.Serial("/dev/ttyUSB0", 9600)
         self.neck = Neck(neck_coef)
-        self.currentEyeDegreeX = 90
-        self.currentEyeDegreeY = 90
-        self.eyeVector = [0, 0]
-        self.eyeLimLeft = []
-        self.eyeLimRight = []
-        self.eyeLimTop = []
-        self.eyeLimBot = []
+        
         
         self.head.write("z0,\n".encode())
 
@@ -22,21 +16,6 @@ class InMoov :
         if track.is_present :
             self.neck.neckLimSensor(track.tracked_object)
             self.neck.animateNeck(self.head)
-
-    def turn_eyes(self) :
-        print(self.currentEyeDegreeY)
-        if (self.eyeVector[0] == 1 and self.currentEyeDegreeX + 5 <= 145) :
-            self.head.write("x5,\n".encode())
-            self.currentEyeDegreeX += 5
-        elif (self.eyeVector[0] == -1 and self.currentEyeDegreeX - 5 >= 85) :
-            self.currentEyeDegreeX -= 5
-            self.head.write("x-5,\n".encode())
-        if (self.eyeVector[1] == 1 and self.currentEyeDegreeY + 5 <= 145) :
-            self.head.write("y5,\n".encode())
-            self.currentEyeDegreeY += 5
-        elif (self.eyeVector[1] == -1 and self.currentEyeDegreeY - 5 >= 20) :
-            self.currentEyeDegreeY -= 5
-            self.head.write("y-5,\n".encode())
 
     def disp_map(self, img) :
         winW = 640
