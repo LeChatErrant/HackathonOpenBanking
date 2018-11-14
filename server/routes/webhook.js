aef97898c9da43cc087f3a903a97e38c7544f141
HECTOR Guillaume <LeChatErrant@localhost.localdomain>
2018-11-14 11:14:45 +0100

    verifying RDV disponiblity on DB

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -24,8 +24,14 @@ const resa = (body, parameter, response) => {
 	return new Promise((resolve, reject) => {
 		const agenda = cache.calendar;
 
-		const date = parameter.day + "/" + (monthRef.indexOf(parameter.month) + 1).toString() + "/2018";
+		const date = parameter.day + "/" + (monthRef.indexOf(parameter.month) + 1).toString().padStart(2, '0') + "/2018";
 		console.log("Date asked: " + date);
+		const existence = Object.keys(agenda).filter(x => agenda[x].data === date);
+		if (existence.length === 0) {
+			response.fulfillmentText = body.queryResult.fulfillmentMessages[1].text.text[0];
+		} else {
+			response.fulfillmentText = body.queryResult.fulfillmentMessages[0].text.text[0];
+		}
 		resolve();
 	});
 }
